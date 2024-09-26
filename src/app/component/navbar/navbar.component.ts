a7e1ee3d922bb152a9a6d7c336787c65bea2888e
Maciej Kubon <maciejk2233@gmail.com>
2024-09-26 19:07:22 +0200

    add colapse nav

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ArrowBackComponent } from '../button/arrow-back/arrow-back.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ArrowBackComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  navPosition="front";
 
+  rollNav(){
+    if(this.navPosition=="back")
+      this.navPosition="front"
+    else
+    this.navPosition="back";
+  }
 }
