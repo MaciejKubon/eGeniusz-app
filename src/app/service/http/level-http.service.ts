9f754ee7e2b76d834f6a68309e96ffd64c234a27
Maciej Kubon <maciejk2233@gmail.com>
2024-10-12 19:35:14 +0200

    creat level service

---
+++ src/app/service/http/level-http.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectHttpService {
+
+  constructor() { }
+
+  private apiUrl ='http://127.0.0.1:8000/api/level';
+
+  http = inject(HttpClient);
+
+  getLevels(){
+    return this.http.get<any>(this.apiUrl);
+  }
+  ediLevel(id:number, subjcet:string){
+    return this.http.put<any>(this.apiUrl+'/'+id,{'name':subjcet})
+  }
+  delateLevel(id:number){
+    return this.http.delete<any>(this.apiUrl+'/'+id)
+  }
+  addLevel(name:string){
+    return this.http.post<any>(this.apiUrl, {'name': name});
+  }
+}
