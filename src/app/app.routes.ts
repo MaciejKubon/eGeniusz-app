12648419e1f8fc9f0ba9d9e7fcdbe536a2d63aad
Maciej Kubon <maciejk2233@gmail.com>
2024-10-12 19:26:46 +0200

    create level page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { SubjectComponent } from './page/subject/subject.component';
 import { NotFoundPageComponent } from './page/not-found-page/not-found-page.component';
 import { ServerErrorComponent } from './page/server-error/server-error.component';
+import { LevelComponent } from './page/level/level.component';
+
 
 export const routes: Routes = [
     { path: 'subject', component: SubjectComponent },
+    {path:'level', component: LevelComponent},
     {path: '404', component:NotFoundPageComponent},
     {path: '500', component:ServerErrorComponent},
     {path: '**', component:NotFoundPageComponent},
