e388285477c2f08bc4bbcddc157a68c0702174bc
Maciej Kubon <maciejk2233@gmail.com>
2024-10-12 20:21:34 +0200

    add form

--- src/app/component/table/level-table/level-table.component.ts
+++ src/app/component/table/level-table/level-table.component.ts
@@ -2,17 +2,40 @@ import { Component } from '@angular/core';
 import { LevelHttpService } from '../../../service/http/level-http.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { level } from '../../../interface/interface';
+import { CommonModule } from '@angular/common';
+import { NewLevelComponent } from '../../form/level/new-level/new-level.component';
+import { CloseButtonComponent } from '../../button/close-button/close-button.component';
+import { AddButtonComponent } from '../../button/add-button/add-button.component';
 
 @Component({
   selector: 'app-level-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NewLevelComponent,
+    CloseButtonComponent,
+    AddButtonComponent,
+  ],
   templateUrl: './level-table.component.html',
-  styleUrl: './level-table.component.scss'
+  styleUrl: './level-table.component.scss',
 })
 export class LevelTableComponent {
-  dataLevel: MatTableDataSource<level>
+  dataLevel: MatTableDataSource<level>;
+  isLoadingResults: boolean = true;
+  visableAddData: boolean = false;
   constructor(private httpLevel: LevelHttpService) {
-    this.dataLevel= new MatTableDataSource([{ id: 0, name: '' }]);
-  } 
+    this.dataLevel = new MatTableDataSource([{ id: 0, name: '' }]);
+  }
+  showAddData(){
+    this.visableAddData = !this.visableAddData;
+  }
+  addLevel(subjectName: string) {
+    this.isLoadingResults = true;
+    // this.httpLevel.addSubject(subjectName).subscribe(() => {
+    //   this.httpLevel.getSubjects().subscribe((data) => {
+    //     this.dataLevel = new MatTableDataSource(data);
+    //     this.isLoadingResults = false;
+    //   });
+    // });
+  }
 }
