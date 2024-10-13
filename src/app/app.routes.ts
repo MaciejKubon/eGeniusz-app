2ffdcbb83d381cea1ea186b53c9a46a9fd6464a2
Maciej Kubon <maciejk2233@gmail.com>
2024-10-13 18:37:56 +0200

    create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { SubjectComponent } from './page/subject/subject.component';
 import { NotFoundPageComponent } from './page/not-found-page/not-found-page.component';
 import { ServerErrorComponent } from './page/server-error/server-error.component';
 import { LevelComponent } from './page/level/level.component';
+import { LoginPageComponent } from './page/login-page/login-page.component';
 
 
 export const routes: Routes = [
+    {path: 'login', component:LoginPageComponent},
     { path: 'subject', component: SubjectComponent },
     {path:'level', component: LevelComponent},
     {path: '404', component:NotFoundPageComponent},
