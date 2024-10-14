b691d2ed0d880bd60c796b7fa80903d2bebf6a98
Maciej Kubon <maciejk2233@gmail.com>
2024-10-14 14:18:42 +0200

    creat login form

--- src/app/page/login-page/login-page.component.ts
+++ src/app/page/login-page/login-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginUserComponent } from '../../component/form/login-user/login-user.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [LoginUserComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
