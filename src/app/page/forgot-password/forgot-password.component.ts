6d7bd03ee77e029973546b3bb67f2786582e7b3a
Maciej Kubon <maciejk2233@gmail.com>
2024-10-15 14:25:39 +0200

    add forgot password page

--- src/app/page/forgot-password/forgot-password.component.ts
+++ src/app/page/forgot-password/forgot-password.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ForgotPasswordFormComponent } from '../../component/form/forgot-password-form/forgot-password-form.component';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [ForgotPasswordFormComponent],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
