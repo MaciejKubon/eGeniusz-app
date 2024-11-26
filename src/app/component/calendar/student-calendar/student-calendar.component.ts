29a587c7a032cfe5763375ef78c924279d76bf15
Maciej Kubon <maciejk2233@gmail.com>
2024-11-26 23:29:32 +0100

    add calender

---
+++ src/app/component/calendar/student-calendar/student-calendar.component.ts
@@ -0,0 +1,105 @@
+import { Component } from '@angular/core';
+import {
+  ClassesStudent,
+  dataRange,
+  studentClass,
+} from '../../../interface/interface';
+import { DatePipe } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ArrowBackComponent } from '../../button/arrow-back/arrow-back.component';
+import { ClassesService } from '../../../service/http/classes.service';
+import { StudentDayComponent } from './student-day/student-day.component';
+
+@Component({
+  selector: 'app-student-calendar',
+  standalone: true,
+  imports: [MatProgressSpinnerModule, ArrowBackComponent, StudentDayComponent],
+  templateUrl: './student-calendar.component.html',
+  styleUrl: './student-calendar.component.scss',
+})
+export class StudentCalendarComponent {
+  position = false;
+  isLoadingResults: boolean = true;
+  classes: studentClass[] = [];
+  hourStart: number = 10;
+  hourEnd: number = 23;
+  hours: string[] = [];
+  dateRangeBlocked = true;
+  dataRangeDate = {
+    start_date: new Date(),
+    end_date: new Date(),
+  };
+  dataRange: dataRange = {
+    start_date: '2024-11-09',
+    end_date: '2024-11-15',
+  };
+  constructor(private studentClasses: ClassesService) {
+    for (let i = this.hourStart; i <= this.hourEnd; i++) {
+      this.hours.push(i + ':00');
+    }
+    this.dataRangeDate.start_date = new Date();
+    this.dataRangeDate.end_date.setDate(
+      this.dataRangeDate.start_date.getDate() + 6
+    );
+    this.setRange();
+  }
+  ngOnInit() {
+    this.refreshData();
+  }
+  refreshData() {
+    this.isLoadingResults = true;
+    this.classes=[];
+    this.studentClasses
+      .getStudentCalendar(this.dataRange)
+      .subscribe((data: studentClass[]) => {
+        data.forEach((e) => {
+          let classe: ClassesStudent[] = [];
+          e.classes.forEach((element) => {
+            classe.push({
+              classDate: element.classDate,
+              classes: {
+                lesson: element.classes.lesson,
+                teacher: element.classes.teacher,
+                confirmed: element.classes.confirmed,
+                start_date: new Date(element.classes.start_date),
+                end_date: new Date(element.classes.end_date),
+              },
+            });
+          });
+          this.classes.push({
+            classDate: e.classDate,
+            classes: classe,
+          });
+        });
+        this.isLoadingResults = false;
+        this.dateRangeBlocked = false;
+        console.log(this.classes);
+        
+      });
+
+  }
+  setRange() {
+    this.dataRange.start_date = new DatePipe('en-US')
+      .transform(this.dataRangeDate.start_date, 'yyyy-MM-dd')
+      ?.toString()!;
+    this.dataRange.end_date = new DatePipe('en-US')
+      .transform(this.dataRangeDate.end_date, 'yyyy-MM-dd')
+      ?.toString()!;
+  }
+  changeRange(days: number) {
+    if (!this.dateRangeBlocked) {
+      this.dateRangeBlocked = true;
+      this.dataRangeDate.start_date.setDate(
+        this.dataRangeDate.start_date.getDate() + days
+      );
+      this.dataRangeDate.end_date.setDate(
+        this.dataRangeDate.end_date.getDate() + days
+      );
+      this.setRange();
+      this.refreshData();
+    }
+  }
+  refDate(ref:boolean){ 
+    this.refreshData();
+  }
+}
---
+++ src/app/component/calendar/student-calendar/student-day/student-day.component.ts
@@ -0,0 +1,71 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { studentClass } from '../../../../interface/interface';
+
+@Component({
+  selector: 'app-student-day',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-day.component.html',
+  styleUrl: './student-day.component.scss',
+})
+export class StudentDayComponent {
+  @Output() refleshData = new EventEmitter<boolean>();
+  @Input() classes: studentClass = {
+    classDate: '',
+    classes: [],
+  };
+  dayNameEmit: string = '';
+  dayName: string = '';
+  hourStart: number = 10;
+  hourEnd: number = 23;
+  dayTime: Date = new Date();
+
+  times: string = '';
+  classesID: number = 0;
+  hours: {
+    hh: string;
+    mm: string[];
+  }[] = [];
+  constructor() {
+    for (let i = this.hourStart; i <= this.hourEnd; i++) {
+      let minutes: string[] = ['00', '15', '30', '45'];
+      this.hours.push({ hh: i.toString(), mm: minutes });
+    }
+  }
+
+  ngOnInit() {
+    this.setName();
+    this.calculateTerms();
+    console.log(this.classes);
+  }
+
+  setName() {
+    let date: string[] = this.classes.classDate.split('-');
+    let day: string = date[2];
+    let month: string = date[1];
+    let year: string = date[0];
+    this.dayName = day + '-' + month + '-' + year;
+    this.dayNameEmit = year + '-' + month + '-' + day;
+    this.dayTime = new Date(this.dayName + ' 10:00:00');
+  }
+
+  calculateTerms() {
+    if (this.classes.classes != null) {
+      this.classes.classes.forEach((e) => {
+        e.classes.diffTime = Math.ceil(
+          Math.abs(
+            e.classes.end_date.getTime() - e.classes.start_date.getTime()
+          ) /
+            (1000 * 60)
+        );
+        e.classes.posTop =
+          2 +
+          Math.ceil(
+            Math.abs(e.classes.start_date.getTime() - this.dayTime.getTime()) /
+              (1000 * 60)
+          ) -
+          540;
+      });
+    }
+  }
+}
